7c4ac504da44f6f49d6612702b3afe22dc7b7058
j4sysiak <j4sysiak@gmail.com>
2026-03-11 14:08:54 +0100

    Day_20_Snake-cz.1

---
+++ Day_20_Snake/main.py
@@ -0,0 +1,25 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0) # Wyłącza animację, żeby wąż nie "skakał"
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update() # Ręczne odświeżanie ekranu
+    time.sleep(0.1) # Spowolnienie pętli
+    snake.move()
+
+screen.exitonclick()
