64f1c9b8b3cab027efe58b2af55ed9c71e51731e
j4sysiak <j4sysiak@gmail.com>
2026-03-11 16:27:04 +0100

    Day_23_Turtle_crossing

--- Day_23_Turtle_crossing/main.py
+++ Day_23_Turtle_crossing/main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard  # Użyj klasy Scoreboard z Ponga/Snake'a
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
